5ffeea0e31872042510b7117c6de410e7da1eca8
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-02-19 09:44:35 +0000

    amended validate token use in router

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -8,7 +8,7 @@ const {
   postArticle,
   deleteArticle
 } = require('../controllers/articles.controller');
-const { validateToken } = require('../controllers/auth.controller')
+const { validateToken } = require('../controllers/auth.controller');
 
 articlesRouter.route('/').get(getAllArticles);
 
@@ -20,7 +20,12 @@ articlesRouter
 
 articlesRouter.route('/:article_id/comments').get(getCommentsByArticle);
 
-articlesRouter.use(validateToken); //need login and password to access all routes
-articlesRouter.route('/').post(postArticle);
-articlesRouter.route('/:article_id/comments').post(postArticleComments);
+//need login and password to access all routes below
+
+articlesRouter.route('/').post(validateToken, postArticle);
+
+articlesRouter
+  .route('/:article_id/comments')
+  .post(validateToken, postArticleComments);
+
 module.exports = articlesRouter;
