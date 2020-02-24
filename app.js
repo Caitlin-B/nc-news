ae9b09c6abce07907dbdc1d54f5ffe54b3704273
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-02-24 13:00:35 +0000

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const apiRouter = require('./routes/api.router');
 const {
   handleCustomErrors,
@@ -10,6 +11,7 @@ const {
 } = require('./errors');
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', apiRouter);
 app.all('/*', (req, res, next) => next({ status: 404, msg: 'Not found' }));
 
